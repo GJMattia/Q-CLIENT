f336ea118efcf086b5ac19ee3d26566e3a93ca11
Greg Mattia <gjmattia@gmail.com>
2024-01-08 19:45:37 -0500

    added live api variable

--- utilities/user-api.js
+++ utilities/user-api.js
@@ -1,5 +1,7 @@
 import sendRequest from './send-request'
-const BASE_URL = 'http://localhost:4741/users'
+// const BASE_URL = 'http://localhost:4741/users'
+const BASE_URL = 'https://q-api.onrender.com/users';
+
 
 export async function signUp(userData) {
     return sendRequest(BASE_URL, 'POST', userData)
